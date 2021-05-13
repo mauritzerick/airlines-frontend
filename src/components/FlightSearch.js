755c2bfe2558926be47afcc9605919092968072b
faithsylvia1 <you@example.com>
2021-05-13 11:14:05 +1000

    add parent Flight Search component

---
+++ src/components/FlightSearch.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import SearchForm from './SearchForm';
+import Result from './Result';
+import axios from 'axios'; //same as $.getJSON - ajax library
+
+class FlightSearch extends Component {
+  constructor() {
+    super();
+    this.state = {flights: [] };
+    // this.fetchFlights = this.fetchFlights.bind(this);
+  }
+
+  render() {//parent's state is becoming a prop for the child
+    return (
+      <div>
+        <SearchForm />
+        <Result />
+      </div>
+    )
+  }
+}
+
+export default FlightSearch;
